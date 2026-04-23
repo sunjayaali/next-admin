4e35fa218267df2a4e97a680fd7915d685ff869d
Ali Sunjaya <ali.sunjayaa@gmail.com>
2026-04-23 23:49:29 +0700

    refactor Textarea component to implement variant support and improve styling

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,18 +1,51 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
 
-function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
+const textareaVariants = cva("textarea", {
+  variants: {
+    color: {
+      neutral: "textarea-neutral",
+      primary: "textarea-primary",
+      secondary: "textarea-secondary",
+      accent: "textarea-accent",
+      info: "textarea-info",
+      success: "textarea-success",
+      warning: "textarea-warning",
+      error: "textarea-error",
+    },
+    size: {
+      xs: "textarea-xs",
+      sm: "textarea-sm",
+      md: "textarea-md",
+      lg: "textarea-lg",
+      xl: "textarea-xl",
+    },
+    variant: {
+      ghost: "textarea-ghost",
+    },
+  },
+});
+
+function Textarea({
+  className,
+  color,
+  size,
+  variant,
+  ...props
+}: React.ComponentProps<"textarea"> & VariantProps<typeof textareaVariants>) {
   return (
     <textarea
       data-slot="textarea"
       className={cn(
-        "flex field-sizing-content min-h-16 w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:aria-invalid:ring-destructive/40",
-        className
+        "w-full",
+        textareaVariants({ color, size, variant }),
+        className,
       )}
       {...props}
     />
-  )
+  );
 }
 
-export { Textarea }
+export { Textarea };
