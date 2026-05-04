02d6fa6f82e2034a5a13695122031053d2382e11
Ali Sunjaya <ali.sunjayaa@gmail.com>
2026-05-04 19:05:29 +0700

    add button component with various styles and icons for enhanced UI

---
+++ app/(v1)/components/buttons/page.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { HeartIcon } from "lucide-react";
+
+export default function Page() {
+  return (
+    <div className="grid">
+      <div className="card shadow">
+        <div className="card-body">
+          <div className="card-title">Buttons</div>
+          <div className="flex flex-wrap gap-2 items-center justify-center">
+            <button className="btn btn-xs">Xsmall</button>
+            <button className="btn btn-sm">Small</button>
+            <button className="btn btn-md">Medium</button>
+            <button className="btn btn-lg">Large</button>
+            <button className="btn btn-xl">Xlarge</button>
+          </div>
+          <div className="divider" />
+
+          <div className="flex flex-wrap gap-2 items-center justify-center">
+            <button className="btn btn-neutral">Neutral</button>
+            <button className="btn btn-primary">Primary</button>
+            <button className="btn btn-secondary">Secondary</button>
+            <button className="btn btn-accent">Accent</button>
+            <button className="btn btn-info">Info</button>
+            <button className="btn btn-success">Success</button>
+            <button className="btn btn-warning">Warning</button>
+            <button className="btn btn-error">Error</button>
+          </div>
+          <div className="divider" />
+
+          <div className="flex flex-wrap gap-2 items-center justify-center">
+            <button className="btn btn-soft btn-neutral">Neutral</button>
+            <button className="btn btn-soft btn-primary">Primary</button>
+            <button className="btn btn-soft btn-secondary">Secondary</button>
+            <button className="btn btn-soft btn-accent">Accent</button>
+            <button className="btn btn-soft btn-info">Info</button>
+            <button className="btn btn-soft btn-success">Success</button>
+            <button className="btn btn-soft btn-warning">Warning</button>
+            <button className="btn btn-soft btn-error">Error</button>
+          </div>
+          <div className="divider" />
+
+          <div className="flex flex-wrap gap-2 items-center justify-center">
+            <button className="btn btn-outline btn-neutral">Neutral</button>
+            <button className="btn btn-outline btn-primary">Primary</button>
+            <button className="btn btn-outline btn-secondary">Secondary</button>
+            <button className="btn btn-outline btn-accent">Accent</button>
+            <button className="btn btn-outline btn-info">Info</button>
+            <button className="btn btn-outline btn-success">Success</button>
+            <button className="btn btn-outline btn-warning">Warning</button>
+            <button className="btn btn-outline btn-error">Error</button>
+          </div>
+          <div className="divider" />
+
+          <div className="flex flex-wrap gap-2 items-center justify-center">
+            <button className="btn btn-dash btn-neutral">Neutral</button>
+            <button className="btn btn-dash btn-primary">Primary</button>
+            <button className="btn btn-dash btn-secondary">Secondary</button>
+            <button className="btn btn-dash btn-accent">Accent</button>
+            <button className="btn btn-dash btn-info">Info</button>
+            <button className="btn btn-dash btn-success">Success</button>
+            <button className="btn btn-dash btn-warning">Warning</button>
+            <button className="btn btn-dash btn-error">Error</button>
+          </div>
+          <div className="divider" />
+
+          <div className="flex flex-wrap gap-2 items-center justify-center">
+            <button className="btn btn-ghost">Ghost</button>
+            <button className="btn btn-link">Link</button>
+            <button className="btn btn-square">
+              <HeartIcon />
+            </button>
+            <button className="btn btn-circle">
+              <HeartIcon />
+            </button>
+            <button className="btn">
+              <HeartIcon />
+            </button>
+            <button className="btn">
+              <span className="loading loading-spinner" />
+            </button>
+            <button className="btn btn-wide">Wide</button>
+            <button className="btn btn-block">Block</button>
+          </div>
+          <div className="divider" />
+        </div>
+      </div>
+    </div>
+  );
+}
