51a2023cc2f339c608ffec571f673c734de3f307
Ali Sunjaya <ali.sunjayaa@gmail.com>
2026-05-12 16:32:27 +0700

    refactor: clean up providers component by removing unused TestModal and simplifying ModalsProvider

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { Button, createTheme, MantineProvider, Text } from "@mantine/core";
-import { ContextModalProps, ModalsProvider } from "@mantine/modals";
+import { createTheme, MantineProvider } from "@mantine/core";
+import { ModalsProvider } from "@mantine/modals";
 import { Notifications } from "@mantine/notifications";
 import {
   QueryClient,
@@ -41,32 +41,12 @@ function MantineProviderWrapper({ children }: { children: React.ReactNode }) {
   return (
     <MantineProvider theme={theme} defaultColorScheme="auto">
       <Notifications />
-      <ModalsProvider
-        modals={{
-          test: TestModal,
-        }}
-      >
-        {children}
-      </ModalsProvider>
+      <ModalsProvider modals={{}}>{children}</ModalsProvider>
     </MantineProvider>
   );
 }
 
-const TestModal = ({
-  context,
-  id,
-  innerProps,
-}: ContextModalProps<{ modalBody: string }>) => (
-  <>
-    <Text size="sm">{innerProps.modalBody}</Text>
-    <Button fullWidth mt="md" onClick={() => context.closeModal(id)}>
-      Close modal
-    </Button>
-  </>
-);
-
 const providers: FC<{ children: React.ReactNode }>[] = [
-  // ThemeProvider,
   MantineProviderWrapper,
   QueryClientProvider,
   agGridProvider,
